9806129a42e90ffddb271c45bc8901b4291947e8
Angel Romero <angel@froged.com>
2022-09-09 19:50:19 +0200

    Improved scroller

--- libs/cooper-ui/src/components/scroller/cui-scroller.component.ts
+++ libs/cooper-ui/src/components/scroller/cui-scroller.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Renderer2 } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'cui-scroller',
@@ -6,11 +7,35 @@ import { ChangeDetectionStrategy, Component, ElementRef, Renderer2 } from '@angu
     styleUrls: ['./cui-scroller.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CuiScrollerComponent {
+export class CuiScrollerComponent implements OnInit, OnDestroy {
 
     constructor(
-        private elementRef: ElementRef,
+        private host: ElementRef,
         private renderer: Renderer2,
     ) { }
+
+    private observer!: ResizeObserver;
+    private overflow = new BehaviorSubject(false);
+    public overflow$ = this.overflow.asObservable();
+
+    ngOnInit(): void {
+        this.observer = new ResizeObserver(() => {
+            const { clientHeight, scrollHeight } = this.host.nativeElement;
+            this.applyClasses(clientHeight !== scrollHeight);
+        });
+        this.observer.observe(this.host.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this.observer.unobserve(this.host.nativeElement);
+    }
+
+    private applyClasses(withOverflow: boolean): void {
+        if (withOverflow) {
+            this.renderer.addClass(this.host.nativeElement, 'with-overflow');
+        } else {
+            this.renderer.removeClass(this.host.nativeElement, 'with-overflow');
+        }
+    }
 
 }
