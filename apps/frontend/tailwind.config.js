8f16de7988770066f59f38cade856d2f17d100cd
Angel Romero <angel@froged.com>
2023-07-19 15:46:25 +0200

    chore: [nx migration] update-16-0-0-add-nx-packages

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,28 +1,26 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const colors = require('tailwindcss/colors');
 const { join } = require('path');
 
 module.exports = {
-    darkMode: 'class',
-    content: [
-        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-        ...createGlobPatternsForDependencies(__dirname),
-    ],
-    theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            primary: colors.sky,
-            accent: colors.pink,
-            success: colors.green,
-            warning: colors.yellow,
-            danger: colors.red,
-            dark: colors.slate,
-            white: colors.white,
-        },
-        extend: {},
+  darkMode: 'class',
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: colors.sky,
+      accent: colors.pink,
+      success: colors.green,
+      warning: colors.yellow,
+      danger: colors.red,
+      dark: colors.slate,
+      white: colors.white,
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
 };
